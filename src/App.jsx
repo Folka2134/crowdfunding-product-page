3621700cfbe60640d82cc01920ee4bd34098bb42
Folka2134 <folka2134@gmail.com>
2023-04-12 11:21:24 +0100

    created banner+nav

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,19 @@
 import "./App.css";
+import logo from "./assets/images/logo.svg";
 
 function App() {
   return (
-    <div className="App">
+    <div className="App h-full">
+      <div className="h-96 bg-mobile-banner md:bg-desktop-banner">
+        <nav className="flex justify-between px-6 md:px-40 lg:px-52 xl:px-72 2xl:px-[20rem] py-10">
+          <img src={logo} alt="logo" />
+          <ul className="flex text-white font-comissioner font-[500]">
+            <li className="px-3">About</li>
+            <li className="px-3">Discover</li>
+            <li className="px-3">Get Started</li>
+          </ul>
+        </nav>
+      </div>
       <div></div>
     </div>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,15 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      backgroundImage: {
+        "desktop-banner": "url('assets/images/image-hero-desktop.jpg')",
+        "mobile-banner": "url('assets/images/image-hero-mobile.jpg')",
+      },
+      fontFamily: {
+        comissioner: ["Comissioner", "sans-serif"],
+      },
+    },
   },
   plugins: [],
 };
